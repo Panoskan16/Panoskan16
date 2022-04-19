a19063752e76c50aae3df708b708f8362ff00120
Panoskan16 <103815034+Panoskan16@users.noreply.github.com>
2022-04-19 23:20:40 +0300

    Update CrossWord_Solver.py

--- CrossWord_Solver.py
+++ CrossWord_Solver.py
@@ -1,4 +1,3 @@
-from sqlalchemy import false, true
 from words_list import english_words_set
 english_letters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 # multiple lists will be used in order to filter the words
